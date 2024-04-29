ae68b2c4a6f6e67450bea5b85759087af0045d03
Mhutashim <billamdmhutashim@gmail.com>
2024-04-29 13:18:20 +0600

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import { Button, ButtonGroup, PopoverArrow } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  GridItem,
+  PopoverArrow,
+  Show,
+} from "@chakra-ui/react";
 function App() {
   return (
-    <Button
-      colorScheme="blue"
-      size="lg"
-      variant="outline"
-      // isLoading
-      // loadingText="submitting"
+    <Grid
+      templateAreas={{
+        base: `"nav" " main"`,
+        lg: `"nav nav" "aside main"`, // equal or larger than ~992px
+      }}
     >
-      Submit
-    </Button>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg" /* below="" */>
+        {/* this will only show in lg screen and higher display */}
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
